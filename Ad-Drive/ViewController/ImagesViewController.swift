5a0a0e61911e3b73664e0273f9c41b040dcd2c30
hamza.salar <hamza.salar@evampsaanga.com>
2022-06-29 14:46:28 +0500

    adding photo library to access your photos from device

--- Ad-Drive/ViewController/ImagesViewController.swift
+++ Ad-Drive/ViewController/ImagesViewController.swift
@@ -7,18 +7,68 @@
 
 import UIKit
 
-class ImagesViewController: UIViewController {
+class ImagesViewController: UIViewController , UIImagePickerControllerDelegate , UINavigationControllerDelegate {
+    
+    var imageView = 0
     @IBOutlet weak var titleLabel: UILabel!
+    @IBOutlet weak var firstImage: UIImageView!
+    @IBOutlet weak var secondImage: UIImageView!
+    @IBOutlet weak var thirdImage: UIImageView!
+    @IBOutlet weak var fourthImage: UIImageView!
     
     override func viewDidLoad() {
         super.viewDidLoad()
-// testing
+        // testing
         // Do any additional setup after loading the view.
     }
     
     @IBAction func imageButtonPressed(_ sender: Any) {
         titleLabel.text = "hello."
+        imageView = 1
+        let imagePickerVC = UIImagePickerController()
+        imagePickerVC.sourceType = .photoLibrary
+        imagePickerVC.delegate = self
+        present(imagePickerVC, animated: true)
     }
+    @IBAction func secondButtonPressed(_ sender: Any) {
+        imageView = 2
+        let imagePickerVC = UIImagePickerController()
+        imagePickerVC.sourceType = .photoLibrary
+        imagePickerVC.delegate = self
+        present(imagePickerVC, animated: true)
+    }
+    @IBAction func thirdButtonPressed(_ sender: Any) {
+        imageView = 3
+        let imagePickerVC = UIImagePickerController()
+        imagePickerVC.sourceType = .photoLibrary
+        imagePickerVC.delegate = self
+        present(imagePickerVC, animated: true)
+    }
+    @IBAction func fourthButtonPressed(_ sender: Any) {
+        imageView = 4
+        let imagePickerVC = UIImagePickerController()
+        imagePickerVC.sourceType = .photoLibrary
+        imagePickerVC.delegate = self
+        present(imagePickerVC, animated: true)
+    }
+    
+    func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [UIImagePickerController.InfoKey : Any]) {
+        picker.dismiss(animated: true, completion: nil)
+        
+        if let image = info[.originalImage] as? UIImage {
+            if imageView == 1{
+                firstImage.image = image
+            } else if imageView == 2{
+                secondImage.image = image
+            } else if imageView == 3{
+                thirdImage.image = image
+            } else if imageView == 4{
+                fourthImage.image = image
+            }
+        }
+    }
+    
+    
     @IBAction func backButtonPressed(_ sender: Any) {
         
     }
@@ -31,5 +81,5 @@ class ImagesViewController: UIViewController {
     }
     
     
-
+    
 }
